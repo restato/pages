a9b249c21d4300eb11ff4a2d73cead96a8e6bcd9
direcision <direcision@gmail.com>
2019-12-20 21:31:27 +0900

    Updates

---
+++ precache-manifest.5e9ee0600eab43ef290c670eff686c18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988e484bd2f82cb31deafa5dfc1fd13d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "444be7e4da0f469bebdd",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c748ab757c3cdb4e036b",
+    "url": "/static/css/main.e29dcaf9.chunk.css"
+  },
+  {
+    "revision": "444be7e4da0f469bebdd",
+    "url": "/static/js/2.a6c07663.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a6c07663.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c748ab757c3cdb4e036b",
+    "url": "/static/js/main.d0421096.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
